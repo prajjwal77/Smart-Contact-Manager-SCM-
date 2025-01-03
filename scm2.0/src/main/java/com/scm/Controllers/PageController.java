695ea7b6447f91893707de9a9576541113a155ec
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2025-01-03 23:35:29 +0530

    Update PageController.java

--- scm2.0/src/main/java/com/scm/Controllers/PageController.java
+++ scm2.0/src/main/java/com/scm/Controllers/PageController.java
@@ -1,11 +1,24 @@
 package com.scm.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.scm.Entities.User;
+import com.scm.Services.impl.UserServiceImpl;
+import com.scm.forms.UserForm;
+
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PageController {
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
 	@RequestMapping("/home")
 	public String home(Model model) {
 		System.out.println("Home page Handler.");
@@ -15,4 +28,55 @@ public class PageController {
 		
 		return "home";
 	}
+
+    @RequestMapping("/about")
+    public String aboutPage(){
+        System.out.println("About page handler.");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String servicePage(){
+        System.out.println("Service page handler.");
+        return "services";
+    }
+
+    @RequestMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register(Model model){
+        UserForm userForm = new UserForm();
+        model.addAttribute("userForm",userForm);
+        return "register";
+    }
+
+    //processing register
+    @RequestMapping(value = "/do-register", method =RequestMethod.POST)
+    public String processingRegister(@ModelAttribute UserForm userForm){
+        //fetch the form data
+        //validate from data
+        //save to database
+        //userService.saveUser(userForm)
+        User user = User.builder()
+        .name(userForm.getName())
+        .email(userForm.getEmail())
+        .password(userForm.getPassword())
+        .about(userForm.getAbout())
+        .phoneNumber(userForm.getPhoneNumber())
+        .build();
+        User savedUser = userServiceImpl.saveUser(user);
+        System.out.println("user saved");
+        //message = "Registration Successfull"
+        //redirection login page
+        return "redirect:/register";
+        //
+    }
 }
