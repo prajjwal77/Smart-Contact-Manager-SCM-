e6ca607aea6c9824aa54fa15b2d8fa778eafbe4a
Prajjwal Yadav <94003570+prajjwal77@users.noreply.github.com>
2025-01-06 23:49:48 +0530

    Update PageController.java

--- scm2.0/src/main/java/com/scm/Controllers/PageController.java
+++ scm2.0/src/main/java/com/scm/Controllers/PageController.java
@@ -9,15 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.scm.Entities.User;
+import com.scm.Services.UserService;
 import com.scm.Services.impl.UserServiceImpl;
 import com.scm.forms.UserForm;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
+
+import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PageController {
     @Autowired
-    private UserServiceImpl userServiceImpl;
+    private UserService userService;
 
 	@RequestMapping("/home")
 	public String home(Model model) {
@@ -60,21 +65,38 @@ public class PageController {
 
     //processing register
     @RequestMapping(value = "/do-register", method =RequestMethod.POST)
-    public String processingRegister(@ModelAttribute UserForm userForm){
+    public String processingRegister(@ModelAttribute UserForm userForm, HttpSession session){
         //fetch the form data
         //validate from data
         //save to database
         //userService.saveUser(userForm)
-        User user = User.builder()
-        .name(userForm.getName())
-        .email(userForm.getEmail())
-        .password(userForm.getPassword())
-        .about(userForm.getAbout())
-        .phoneNumber(userForm.getPhoneNumber())
-        .build();
-        User savedUser = userServiceImpl.saveUser(user);
+
+        // User user = User.builder()
+        // .name(userForm.getName())
+        // .email(userForm.getEmail())
+        // .password(userForm.getPassword())
+        // .about(userForm.getAbout())
+        // .phoneNumber(userForm.getPhoneNumber())
+        // .build();
+        User user = new User();
+        user.setName(userForm.getName());
+        user.setEmail(userForm.getEmail());
+        user.setPassword(userForm.getPassword());
+        user.setAbout(userForm.getAbout());
+        user.setPhoneNumber(userForm.getPhoneNumber());
+        user.setProfilePic("");
+
+    
+        User savedUser = userService.saveUser(user);
         System.out.println("user saved");
+       
         //message = "Registration Successfull"
+
+        //add the message
+        Message message = Message.builder().content("Registration Successful").type(MessageType.green).build();
+
+        session.setAttribute("message", message);
+
         //redirection login page
         return "redirect:/register";
         //
